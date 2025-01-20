65503d1bda2f8c47e0138e226916aed647ba75d3
gxt_kt <gxt_kt@163.com>
2025-01-20 13:38:28 +0800

    fix: solve bug that change head title content

--- scripts/common/postDesc.js
+++ scripts/common/postDesc.js
@@ -3,8 +3,14 @@
 const { stripHTML, truncate } = require('hexo-util')
 
 // Truncates the given content to a specified length, removing HTML tags and replacing newlines with spaces.
+// 改成下面的用来修复修改标题带入内容的bug
 const truncateContent = (content, length) => {
-  return truncate(stripHTML(content), { length, separator: ' ' }).replace(/\n/g, ' ')
+  // 移除所有 <style> 标签及其内容
+  const contentWithoutStyles = content.replace(/<style[\s\S]*?>[\s\S]*?<\/style>/g, '');
+  // 移除其他 HTML 标签
+  const strippedContent = stripHTML(contentWithoutStyles);
+  // 截取内容并替换换行符
+  return truncate(strippedContent, { length, separator: ' ' }).replace(/\n/g, ' ');
 }
 
 // Generates a post description based on the provided data and theme configuration.
