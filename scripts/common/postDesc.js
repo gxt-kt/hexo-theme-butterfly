26a3a03c494159fa805e7705131a7e8c8e1ad823
gxt_kt <gxt_kt@163.com>
2025-01-24 17:30:49 +0800

    fix add head #

--- scripts/common/postDesc.js
+++ scripts/common/postDesc.js
@@ -7,8 +7,10 @@ const { stripHTML, truncate } = require('hexo-util')
 const truncateContent = (content, length) => {
   // 移除所有 <style> 标签及其内容
   const contentWithoutStyles = content.replace(/<style[\s\S]*?>[\s\S]*?<\/style>/g, '');
+  // 移除 <span class="heading-prefix"> 中的内容（即额外添加的 # 前缀）
+  const contentWithoutPrefix = contentWithoutStyles.replace(/<span class="heading-prefix">#+<\/span>/g, '');
   // 移除其他 HTML 标签
-  const strippedContent = stripHTML(contentWithoutStyles);
+  const strippedContent = stripHTML(contentWithoutPrefix);
   // 截取内容并替换换行符
   return truncate(strippedContent, { length, separator: ' ' }).replace(/\n/g, ' ');
 }
